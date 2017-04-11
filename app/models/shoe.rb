d9cdb0fa9072a3d5bcec26916bbcb8bbbb9f5435
Biz Metzler <metzler.elizabetha@gmail.com>
2017-04-10 22:06:05 -0600

    added data validations to shoe model

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -3,4 +3,10 @@ class Shoe < ApplicationRecord
   has_many :rentals
 
   mount_uploader :photo, ShoePhotoUploader
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :description, presence: true, uniqueness: true, length: { minimum: 10 }
+  validates :brand, presence: true, inclusion: { in: ["Nike", "Puma", "Supreme", "Adidas", "Yohji Yamamoto", "New Balance"], allow_nil: false }
+  validates :size, presence: true
+  validates :unit_price, presence: true
 end
