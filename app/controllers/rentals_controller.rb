8b8c01f310af65bfed7de37d7d8d3e5c1caebeae
Biz Metzler <metzler.elizabetha@gmail.com>
2017-04-10 20:17:29 -0600

    update action method for rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,13 +1,33 @@
 class RentalsController < ApplicationController
   def index
+    @rentals = Rental.all
   end
 
   def show
+    @rental = Rental.find(params[:id])
   end
 
   def new
+    @rental = Rental.new
+    @shoe = Shoe.new
   end
 
+  # POST   /shoes/:shoe_id/rentals(.:format)     rentals#create
   def create
+    @rental = Rental.new(user_params)
+    @shoe = Shoe.new(params[:shoe_id])
+    @rental.shoe = @shoe
+
+    if @rental.save
+      redirect_to rental_path(@rental)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:rental).permit(:start_date, :end_date, :total_cost, :status)
   end
 end
