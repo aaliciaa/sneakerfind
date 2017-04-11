199193b647fba904803f702bf88441e2c489fbf4
Biz Metzler <metzler.elizabetha@gmail.com>
2017-04-10 22:29:33 -0600

    fixed up rental#create method and added data validations for rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,14 @@
 class Rental < ApplicationRecord
   belongs_to :shoe
   belongs_to :renter, class_name: "User"
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_cost, presence: true
+  validates :status, presence: true, inclusion: { in: ["pending", "accepted", "declined", "complete"], allow_nil: false }
 end
+
+
+
+
+
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -4,9 +4,5 @@ class Shoe < ApplicationRecord
 
   mount_uploader :photo, ShoePhotoUploader
 
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
-  validates :description, presence: true, uniqueness: true, length: { minimum: 10 }
-  validates :brand, presence: true, inclusion: { in: ["Nike", "Puma", "Supreme", "Adidas", "Yohji Yamamoto", "New Balance"], allow_nil: false }
-  validates :size, presence: true
-  validates :unit_price, presence: true
+
 end
